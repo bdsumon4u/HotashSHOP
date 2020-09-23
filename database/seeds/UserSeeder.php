e0c08989328acb18f9cd8498b6a0d2a212a8e825
Sumon Ahmed <bdsumon4u@gmail.com>
2020-09-23 23:12:12 +0600

    UserFactory, UserSeeder, OrderFactory

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\User;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -11,12 +13,18 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        User::create([
-            'name' => 'Sumon Ahmed',
-            'email' => 'bdsumon4u@gmail.com',
-            'password' => Hash::make('password'),
-        ]);
+        for ($i=0; $i < 20; $i++) { 
+            User::create([
+                'name' => $faker->name,
+                'email' => "user{$i}@cyber32.com",
+                'phone_number' => $faker->unique()->e164PhoneNumber,
+                'address' => $faker->address,
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ]);
+        }
     }
 }
