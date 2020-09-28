5635650537da8719a3d8a8e71ad1a85f9bff1888
Sumon Ahmed <bdsumon4u@gmail.com>
2020-09-28 10:40:16 +0600

    Scout Outside Terminal

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
         App\Providers\ComposerServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\ScoutServiceProvider::class,
 
     ],
 
